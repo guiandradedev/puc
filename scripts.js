d4c8672ae622825be2d2dca123f2094974830d99
Guilherme Andrade <42524262+guiandradedev@users.noreply.github.com>
2023-02-28 11:07:47 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,38 @@
+const banner = new Swiper('.swiper.slider-banner', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  const ofertas = new Swiper('.swiper.slider-ofertas', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
